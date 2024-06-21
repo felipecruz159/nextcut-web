99070948eea938ae80d2dbb65cdd19c362e5d4e3
RafaGms <faelgomes2003@gmail.com>
2024-06-21 14:16:49 -0300

    ✨feat(added): add header-singin component

---
+++ src/app/(home)/_components/header-singin.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "../../_components/ui/card";
+import { DoorOpen } from 'lucide-react';
+
+const HeaderSingin = () => {
+   return (
+      <Card className="rounded-none">
+         <CardContent className="flex justify-between pb-0 py-6">
+            <div>Logo</div>
+            <div><Button className="flex gap-1 text-primary border-primary" size={"sm"} variant={"outline"}>
+               Entrar <DoorOpen size={18} />
+            </Button></div>
+         </CardContent>
+      </Card>
+   );
+}
+
+export default HeaderSingin;
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,9 @@
 const axios = require('axios');
 import { useEffect, useState } from "react";
 import { axiosInstance } from "../_helpers/axios-instance";
+import HeaderSingin from "./_components/header-singin";
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
 export default function Home() {
   const [barbershop, setBarbershop] = useState([])
@@ -15,8 +18,30 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="">
+    <div>
+      <HeaderSingin />
 
-    </main>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-screen-xl mx-auto">
+        <div className="mt-4 px-5">
+          <h2 className="text-xl font-bold font-light">Olá, faça seu login</h2>
+          <p className="capitalize text-muted-foreground text-sm">{format(new Date(), "EEEE',' d 'de' yyyy", {
+            locale: ptBR
+          })}</p>
+          <p className=""></p>
+          <div className="px-5 mt-6">
+            {/* TODO: Make input seach */}
+          </div>
+        </div>
+
+        {/* Card hidden below the "md" splint */}
+        <div className="hidden md:flex">
+          {/* TODO: Make card carousel */}
+        </div>
+      </div>
+
+      <div>
+
+      </div>
+    </div>
   );
 }
