2ca10f9d9d9a38d355f1f95c31ace80057e3311d
RafaGms <faelgomes2003@gmail.com>
2024-06-21 11:45:49 -0300

    ⚙️chore(refactoring) improvement in the axios code

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,29 +1,22 @@
+'use client'
 const axios = require('axios');
+import { useEffect, useState } from "react";
+import { axiosInstance } from "../_helpers/axios-instance";
 
-const getData = async () => {
-  const response = await axios.get('http://localhost:3333/api/service')
-  return response.data
-}
+export default function Home() {
+  const [barbershop, setBarbershop] = useState([])
 
-type service = {
-  id: string,
-  barbershopId: string,
-  name: string,
-  description: string,
-  price: number,
-  imageUrl: string,
-}
-
-export default async function Home() {
-  const getServices = await getData();
-  console.log({ getServices });
-
+  useEffect(() => {
+    axiosInstance.get('barbershop').then((res) => {
+      setBarbershop(res.data)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {getServices.map((item: service) => (
-        <p key={item.id}>{item.name}</p>
-      ))}
+    <main className="">
+
     </main>
   );
 }
