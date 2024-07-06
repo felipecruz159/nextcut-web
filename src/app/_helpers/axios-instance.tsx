7a72c6145e3ef9f79aaeafc3decbba4bf8d62d0a
felipecruz159 <felipe.cruz159@hotmail.com>
2024-07-05 23:07:53 -0300

    🔒️chore: add dotenv to axios instance

--- src/app/_helpers/axios-instance.tsx
+++ src/app/_helpers/axios-instance.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+require("dotenv").config();
 export const axiosInstance = axios.create({
-   baseURL: 'http://localhost:3333/api/'
+   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 })
